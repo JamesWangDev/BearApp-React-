f9e7c9b3abed16f5793edb5871c4291e6feddad5
jameswang <james.wang.dev712@gmail.com>
2020-02-13 07:26:40 +1000

    homepage update to test now.sh autodeployment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,7 +12,7 @@ const Home = () => (
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
+      <h1 className="title">Welcome to Next.js!!!!</h1>
       <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
