8dfb9b0b96f8af529fedf449f40f5fbcf69fdbd9
jameswang <james.wang.dev712@gmail.com>
2020-02-12 17:44:56 +1000

    testing eslint and prettier pre-commit hooks across fontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) = res.send('Hello world!'))
 
 app.listen(port, () => console.log('Server listening on port 3001'))
