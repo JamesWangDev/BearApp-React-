237ada83a993f855d0a06b9892b0e35989a340c2
dastrong <danielstrong.ds@gmail.com>
2020-02-13 16:16:07 +0900

    updated global error handler to use a more appropriate interface

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Response, Request, NextFunction } from "express";
+import express, { ErrorRequestHandler } from "express";
 import createError from "http-errors";
 import morganBody from "morgan-body";
 // needed to connect to DB later
@@ -25,22 +25,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // use all routes exported from the routes folder
 app.use("/api", routes);
+
 // used to catch any routes not found
 app.use((req, _, next) => {
-  next(createError(404, `Invalid route!!__: ${req.url}`));
+  next(createError(404, `Invalid route: ${req.url}`));
 });
+
 // global error handler
 // access by passing err with the next callback ie) next(err)
-app.use(
-  (
-    error: { status: number; message: string },
-    _req: Request,
-    res: Response,
-    _next: NextFunction
-  ) => {
-    const { status = 400, message } = error;
-    res.status(status).json({ message });
-  }
-);
+const GlobalErrorHandler: ErrorRequestHandler = (
+  { status, message }: { status: number; message: string },
+  _req,
+  res,
+  _next
+) => {
+  res.status(status || 400).json({ message });
+};
+app.use(GlobalErrorHandler);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
