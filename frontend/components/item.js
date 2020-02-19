d23373a18d909bd4d8255c5c1e1f28e59a153b0c
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2020-02-19 16:18:12 +0530

    Add item delete

--- frontend/components/item.js
+++ frontend/components/item.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { itemType } from "../types";
+import fetch from "isomorphic-fetch";
+import { getBackendAPI } from "../utils";
+
+const handleDelete = async id => {
+  await fetch(`${getBackendAPI("item")}/${id}`, {
+    method: "DELETE",
+  });
+};
 
 function Item({
+  _id,
   name,
   description,
   link,
@@ -19,8 +28,14 @@ function Item({
       <div>Is Reserved: {isReserved ? "true" : "false"}</div>
       <div>Price: {price}</div>
       <img alt={`${name} image`} height={100} src={image} />
+      <button className="deleteButton" onClick={() => handleDelete(_id)}>
+        Delete
+      </button>
       <style jsx>{`
         .itemContainer {
+          border: 1px solid black;
+        }
+        .deleteButton {
           border: 1px solid black;
         }
       `}</style>
