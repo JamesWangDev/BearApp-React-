fb1cbc92a5786436599b034be1432e032e63a583
jameswang <james.wang.dev712@gmail.com>
2020-02-19 13:16:47 +1000

    Added form for creating an item

--- frontend/utils/index.js
+++ frontend/utils/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-export const getBackendAPI = () =>
+export const getBackendAPI = route =>
   process.env.NODE_ENV === "production"
-    ? "https://v16-bears-04-wedding-registry.herokuapp.com/api"
-    : "http://localhost:5000/api";
+    ? `https://v16-bears-04-wedding-registry.herokuapp.com/api/${route}`
+    : `http://localhost:5000/api/${route}`;
