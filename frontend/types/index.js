50c62b8a6f4b0a5612611f0a08f461fedab66e79
jameswang <james.wang.dev712@gmail.com>
2020-02-21 14:44:51 +1000

    Added styles for form text input fields and a button

--- frontend/types/index.js
+++ frontend/types/index.js
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
 
 export const itemType = {
-  name: PropTypes.string,
-  description: PropTypes.string,
-  price: PropTypes.number,
-  link: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  link: PropTypes.string.isRequired,
   image: PropTypes.string,
-  isPurchased: PropTypes.bool,
-  isReserved: PropTypes.bool,
+  isPurchased: PropTypes.bool.isRequired,
+  isReserved: PropTypes.bool.isRequired,
   addedOn: PropTypes.string,
   reservedOn: PropTypes.string,
   purchasedOn: PropTypes.string,
 };
+
+export const formErrorType = PropTypes.shape({
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  ref: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }),
+  ]).isRequired,
+});
