d7c4c58456b6404e03b0b3f540214f2f0fa18bb1
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-01 15:57:52 +0900

    updated/added registry model validation tests

---
+++ backend/src/models/Registry/registry-examples.ts
@@ -0,0 +1,125 @@
+export const validRegistry = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidRegistry = {
+  //title: "Rose and Mel's Wedding",
+  // description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  // p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidTitle = {
+  //title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidDescription = {
+  title: "Rose and Mel's Wedding",
+  // description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidP1FullName = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  // p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidP2FullName = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  // p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidEmail = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  // email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidUserId = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  // userId: "qqqq",
+  customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
+
+export const invalidCustomUrl = {
+  title: "Rose and Mel's Wedding",
+  description: "Come celebrate",
+  userId: "qqqq",
+  // customUrl: "RoseAndMel",
+  weddingDate: "2020-02-26T09:42:50.667Z",
+  tyMessage: "Thanks for the Gift",
+  p1FullName: "Rose Maf",
+  p2FullName: "Mel Ras",
+  phoneNumber: 3217890,
+  email: "rose@gmail.com",
+  coverImage: "https://bit.ly/2Pr0xeQ",
+};
