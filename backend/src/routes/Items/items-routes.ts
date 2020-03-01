aa95bba54b64dcbf47786f6d15a0647b0a157f0d
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-01 17:03:06 +0900

    removed unused route/controller

--- backend/src/routes/Items/items-routes.ts
+++ backend/src/routes/Items/items-routes.ts
@@ -8,7 +8,6 @@ router.post("/item", Item.createItem);
 router.get("/item/:itemId", Item.getOneItem);
 router.put("/item/:itemId", Item.updateOneItem);
 router.delete("/item/:itemId", Item.deleteOneItem);
-router.get("/item/:registryId", Item.getRegistryItems); // not tested
 router.delete("/item/:registryId", Item.deleteRegistryItems); // not tested
 
 export default router;
