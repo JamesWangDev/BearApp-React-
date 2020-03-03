1eab988b942a4c72dd440dd23f40a2c222396070
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-03 12:24:27 +0900

    updated item test routes and added deleteMultipleItems tests

--- backend/src/routes/Items/items.test.ts
+++ backend/src/routes/Items/items.test.ts
@@ -1,34 +1,46 @@
-// registryId hasn't been turned on yet, so there are no tests for ...
-// ... it, therefore 'getRegistryItems' and 'deleteRegistryItems' ...
-// ... controllers still need to still be tested
-
 import { request, dbSetup } from "../../test";
 import { validItem, invalidItem } from "../../models/Item";
+import { validRegistry } from "../../models/Registry";
 
 dbSetup();
 
-const getAllItems = async () => await request.get("/api/items").expect(200);
+const getAllItems = async () => await request.get("/api/item/all").expect(200);
 
 const getItem = async (itemId: string, status: number) =>
   await request.get(`/api/item/${itemId}`).expect(status);
 
-const createItem = async (item: {}, status: number) =>
+const createItem = async (registryId: string, item: {}, status: number) =>
   await request
-    .post("/api/item")
+    .post(`/api/item/registry/${registryId}`)
     .send(item)
     .expect(status);
+
+const createRegistry = async (registry: {}, status: number) =>
+  await request
+    .post("/api/registry")
+    .send(registry)
+    .expect(status);
+
+const getRegistrybyUrl = async (customUrl: string, status: number) =>
+  await request.get(`/api/registry/${customUrl}`).expect(status);
 
 describe("Item Endpoint Tests", () => {
   describe("Item Creation", () => {
     test("expected to successfully create one item", async () => {
-      const resp = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const resp = await createItem(registryId, validItem, 201);
       expect(resp.body.name).toBe(validItem.name);
       expect(resp.body.description).toBe(validItem.description);
       expect(resp.body.price).toBe(validItem.price);
     });
 
     test("expected to unsuccessfully create one item", async () => {
-      const resp = await createItem(invalidItem, 400);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const resp = await createItem(registryId, invalidItem, 400);
       expect(resp.body.message).toBe(
         "Item validation failed: description: Item description required, price: Path `price` (-1) is less than minimum allowed value (0)."
       );
@@ -37,8 +49,11 @@ describe("Item Endpoint Tests", () => {
 
   describe("Get Item/s", () => {
     test("expected to get all items in DB", async () => {
-      const newItem1 = await createItem(validItem, 201);
-      const newItem2 = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem1 = await createItem(registryId, validItem, 201);
+      const newItem2 = await createItem(registryId, validItem, 201);
 
       const allItems = await getAllItems();
       expect(allItems.body).toHaveLength(2);
@@ -51,7 +66,10 @@ describe("Item Endpoint Tests", () => {
     });
 
     test("expected to get a single item by itemID", async () => {
-      const newItem = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem = await createItem(registryId, validItem, 201);
       const itemId = newItem.body._id;
 
       const foundItem = await getItem(itemId, 200);
@@ -71,7 +89,10 @@ describe("Item Endpoint Tests", () => {
 
   describe("Update/Delete Items", () => {
     test("expected to delete a single item", async () => {
-      const newItem = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem = await createItem(registryId, validItem, 201);
       const itemId = newItem.body._id;
 
       const foundItem = await getItem(itemId, 200);
@@ -80,7 +101,7 @@ describe("Item Endpoint Tests", () => {
       expect(foundItem.body).toStrictEqual(newItem.body);
 
       const deletedItem = await request
-        .delete(`/api/item/${itemId}`)
+        .delete(`/api/item/${itemId}/registry/${registryId}`)
         .expect(200);
       expect(deletedItem.body).not.toBeNull();
 
@@ -89,7 +110,10 @@ describe("Item Endpoint Tests", () => {
     });
 
     test("expected to unsuccessfully delete a single item", async () => {
-      const newItem = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem = await createItem(registryId, validItem, 201);
       const itemId = newItem.body._id;
 
       const foundItem = await getItem(itemId, 200);
@@ -98,7 +122,7 @@ describe("Item Endpoint Tests", () => {
       expect(foundItem.body).toStrictEqual(newItem.body);
 
       const resp = await request
-        .delete("/api/item/invalid-id-string")
+        .delete(`/api/item/invalid-id-string/registry/${registryId}`)
         .expect(400);
       expect(resp.body.message).toBe(
         // eslint-disable-next-line quotes
@@ -106,8 +130,108 @@ describe("Item Endpoint Tests", () => {
       );
     });
 
+    test("expected to successfully delete multiple items", async () => {
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+      const registryUrl = registry.body.customUrl;
+
+      const newItem1 = await createItem(registryId, validItem, 201);
+      const newItem2 = await createItem(registryId, validItem, 201);
+      const itemId1 = newItem1.body._id;
+      const itemId2 = newItem2.body._id;
+      const arrayOfIds = JSON.stringify([itemId1, itemId2]);
+
+      const resp = await request
+        .delete(`/api/item/registry/${registryId}`)
+        .send({ arrayOfIds })
+        .expect(200);
+      expect(resp.body.message).toBe(
+        "Updated registry items and deleted 2/2 items"
+      );
+
+      // check that the items don't exist now
+      const item1 = await getItem(itemId1, 404);
+      const item2 = await getItem(itemId2, 404);
+      expect(item1.body.message).toBe(`Item (${itemId1}) not found`);
+      expect(item2.body.message).toBe(`Item (${itemId2}) not found`);
+
+      // check that the items ids aren't in the registry items array
+      const updatedRegistry = await getRegistrybyUrl(registryUrl, 200);
+      expect(updatedRegistry.body.items).not.toContain(itemId1);
+      expect(updatedRegistry.body.items).not.toContain(itemId2);
+    });
+
+    test("expected to unsuccessfully delete multiple items - missing arrayOfIds", async () => {
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      await createItem(registryId, validItem, 201);
+      await createItem(registryId, validItem, 201);
+
+      const resp = await request
+        .delete(`/api/item/registry/${registryId}`)
+        .expect(400);
+      expect(resp.body.message).toBe("Please pass arrayOfIds in the body");
+    });
+
+    test("expected to unsuccessfully delete multiple items - empty arrayOfIds", async () => {
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      await createItem(registryId, validItem, 201);
+      await createItem(registryId, validItem, 201);
+      const arrayOfIds = JSON.stringify([]);
+
+      const resp = await request
+        .delete(`/api/item/registry/${registryId}`)
+        .send({ arrayOfIds })
+        .expect(400);
+      expect(resp.body.message).toBe("You didn't put any _id's in arrayOfIds");
+    });
+
+    test("expected to unsuccessfully delete multiple items - invalid id in arrayOfIds", async () => {
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem1 = await createItem(registryId, validItem, 201);
+      await createItem(registryId, validItem, 201);
+      const itemId1 = newItem1.body._id;
+      const arrayOfIds = JSON.stringify([itemId1, "invalidId"]);
+
+      const resp = await request
+        .delete(`/api/item/registry/${registryId}`)
+        .send({ arrayOfIds })
+        .expect(400);
+      expect(resp.body.message).toBe(
+        "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+      );
+    });
+
+    test("expected to unsuccessfully delete multiple items - invalid registry", async () => {
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+      const invalidRegistryId = registryId + "2";
+
+      const newItem1 = await createItem(registryId, validItem, 201);
+      const newItem2 = await createItem(registryId, validItem, 201);
+      const itemId1 = newItem1.body._id;
+      const itemId2 = newItem2.body._id;
+      const arrayOfIds = JSON.stringify([itemId1, itemId2]);
+
+      const resp = await request
+        .delete(`/api/item/registry/${invalidRegistryId}`)
+        .send({ arrayOfIds })
+        .expect(400);
+      expect(resp.body.message).toBe(
+        `Cast to ObjectId failed for value \"${invalidRegistryId}\" at path \"_id\" for model \"Registry\"`
+      );
+    });
+
     test("expected to successfully update an item", async () => {
-      const newItem = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem = await createItem(registryId, validItem, 201);
       const itemId = newItem.body._id;
 
       const foundItem = await getItem(itemId, 200);
@@ -130,7 +254,10 @@ describe("Item Endpoint Tests", () => {
     });
 
     test("expected to unsuccessfully update an item", async () => {
-      const newItem = await createItem(validItem, 201);
+      const registry = await createRegistry(validRegistry, 201);
+      const registryId = registry.body._id;
+
+      const newItem = await createItem(registryId, validItem, 201);
       const itemId = newItem.body._id;
 
       const foundItem = await getItem(itemId, 200);
