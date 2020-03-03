436b472d88b1fdf9e4ea638113b8a1521865bf08
Daniel Strong <danielstrong.ds@gmail.com>
2020-03-03 14:33:05 +0900

    removed 'not tested' comment

--- backend/src/routes/Items/items-routes.ts
+++ backend/src/routes/Items/items-routes.ts
@@ -8,6 +8,6 @@ router.get("/:itemId", Item.getOneItem);
 router.put("/:itemId", Item.updateOneItem); // isOwner _ isAdmin
 router.delete("/:itemId/registry/:registryId", Item.deleteOneItem); // isOwner _ isAdmin
 router.post("/registry/:registryId", Item.createItem); // isUser
-router.delete("/registry/:registryId", Item.deleteMultipleItems); // isOwner _ isAdmin // not tested
+router.delete("/registry/:registryId", Item.deleteMultipleItems); // isOwner _ isAdmin
 
 export default router;
