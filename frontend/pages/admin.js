09da677ccb7617aa3aef599702ef66cb1eb12060
jameswang <james.wang.dev712@gmail.com>
2020-03-05 10:06:37 +1000

    Added registry details form

--- frontend/pages/admin.js
+++ frontend/pages/admin.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import useSWR from "swr";
 import { withAuth, withLoginRequired } from "use-auth0-hooks";
-import PropTypes from "prop-types";
 import RegistryIcon from "@iconscout/react-unicons/icons/uil-diary";
 import AdminPage from "../components/AdminPage";
-import { fetchIt } from "../utils";
+import InputText from "../components/InputText";
+import Button from "../components/Button";
 import { authType } from "../types";
 
-const Admin = ({ registry, auth }) => {
-  const { data } = useSWR("/registry", { initialData: registry });
-  console.log("data: ", data);
+const Admin = ({ auth }) => {
+  const { data } = useSWR("/registry/RoseAndMel", { initialData: {} });
   console.log("auth: ", auth);
+  // console.log("data: ", data);
+  const { register, handleSubmit, errors } = useForm({
+    defaultValues: {
+      ...data,
+      "p1-full-name": data.p1FullName,
+      "p2-full-name": data.p2FullName,
+      "phone-number": data.phoneNumber,
+      "ty-message": data.tyMessage,
+      "custom-url": data.customUrl,
+    },
+  });
+  const onSubmit = formData => {
+    console.log("formData: ", formData);
+  };
   return (
     <AdminPage>
       <header className="header flex py-10 px-5 text-size text-xl">
@@ -21,6 +35,65 @@ const Admin = ({ registry, auth }) => {
           <h1>Registry details</h1>
         </div>
       </header>
+      <div className="p-10">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <InputText
+            id="title"
+            error={errors.title}
+            ref={register({ required: "Title is required" })}
+          >
+            Title
+          </InputText>
+          <InputText
+            id="description"
+            error={errors.description}
+            type="textarea"
+            ref={register}
+          >
+            Description
+          </InputText>
+          <InputText
+            id="p1FullName"
+            error={errors.p1FullName}
+            ref={register({ required: "Partner 1 name is required" })}
+          >
+            Partner 1 full name
+          </InputText>
+          <InputText
+            id="p2FullName"
+            error={errors.p2FullName}
+            ref={register({ required: "Partner 2 name is required" })}
+          >
+            Partner 2 full name
+          </InputText>
+          <InputText
+            id="email"
+            error={errors.email}
+            ref={register({ required: "Email is required" })}
+          >
+            Email
+          </InputText>
+          <InputText id="phoneNumber" error={errors.phone} ref={register}>
+            Phone number
+          </InputText>
+          <InputText
+            id="tyMessage"
+            type="text"
+            error={errors.tyMessage}
+            ref={register}
+          >
+            Thank you message
+          </InputText>
+          <InputText
+            id="customUrl"
+            error={errors.customUrl}
+            ref={register({ required: "Custom URL is required" })}
+          >
+            Custom URL
+          </InputText>
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
       <style jsx>{`
         .header .header__icon {
           display: flex;
@@ -40,13 +113,19 @@ const Admin = ({ registry, auth }) => {
   );
 };
 
-Admin.getInitialProps = async () => {
-  const registry = await fetchIt("/registry");
-  return { registry };
-};
+// weddingDate : {
+//     type: Date,
+// },
+// userId: {
+//     type: String,
+//     required: [true, "UserId is required"],
+// },
+// coverImage: {
+//     type: String,
+//     default: "https://bit.ly/2Pr0xeQ"
+// }
 
 Admin.propTypes = {
-  registry: PropTypes.any,
   auth: authType,
 };
 
